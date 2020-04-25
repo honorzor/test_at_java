af5cfb4e45f9e56e910e250265f75bd063b748a3
honorzor <ioanntrapeznikov@gmail.com>
2020-04-25 19:58:26 +0300

    Add PageObject pattern

--- src/test/java/ru/honorzor/EuroDollar.java
+++ src/test/java/ru/honorzor/EuroDollar.java
@@ -1,50 +1,36 @@
 package ru.honorzor;
 
-import com.codeborne.selenide.ElementsCollection;
-import org.openqa.selenium.By;
+
+import google.BankSite;
+import google.GooglePage;
+import google.GoogleSearchResult;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.page;
 
 public class EuroDollar {
     private static final String URL = "https://www.google.com";
-    private static final String EXCEPTEDLINK = "https://www.open.ru/";
+    private static final String EXCEPTEDLINK = "www.open.ru";
 
-    @BeforeMethod
-    public void signUp() {
-        open(URL);
+    @Test
+    public void ratesUsdEuro() {
+        GooglePage googlePage = GooglePage.openBrowser(URL);
+
+        GoogleSearchResult list = googlePage.search("открытие");
+
+        Assert.assertTrue(list.results().toString().contains(EXCEPTEDLINK));
+
+        $(".r > a").click();
+
+        BankSite page = page(BankSite.class);
+
+        Assert.assertTrue(page.infoBuyUSD() < page.infoSellUSD());
+        Assert.assertTrue(page.infoBuyEURO() < page.infoSellEURO());
+
+
     }
 
-    @Test(priority = 2)
-    public void checkUsdUero() {
-        element(By.name("q")).setValue("открытие").pressEnter();
-        ElementsCollection elements = elements("#search .g");
-        elements.first().find(".r>a").click();
-        String bUSD = element(By.xpath("//*[@id=\"main\"]/div/div/div[8]/section/div/div/div[1]/div/div/div" +
-                "/div/div[2]/table/tbody/tr[2]/td[2]/div/span")).getText().replace(",", ".");
-        String sUSD = element(By.xpath("//*[@id=\"main\"]/div/div/div[8]/section/div/div/div[1]/div/div/div" +
-                "/div/div[2]/table/tbody/tr[2]/td[4]/div/span")).getText().replace(",", ".");
-        String bEURO = element(By.xpath("//*[@id=\"main\"]/div/div/div[8]/section/div/div/div[1]/div/div/div" +
-                "/div/div[2]/table/tbody/tr[3]/td[2]/div/span")).getText().replace(",", ".");
-        String sEURO = element(By.xpath("//*[@id=\"main\"]/div/div/div[8]/section/div/div/div[1]/div/div/" +
-                "div/div/div[2]/table/tbody/tr[3]/td[4]/div/span")).getText().replace(",", ".");
 
-
-        double buyUSD = Double.parseDouble(bUSD);
-        double saleUSD = Double.parseDouble(sUSD);
-        double buyEURO = Double.parseDouble(bEURO);
-        double saleEURO = Double.parseDouble(sEURO);
-
-        Assert.assertTrue(saleUSD > buyUSD);
-        Assert.assertTrue(saleEURO > buyEURO);
-    }
-
-    @Test(priority = 1)
-    public void checkSearcd() {
-        element(By.name("q")).setValue("открытие").pressEnter();
-        Assert.assertEquals((element(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/a")).getAttribute("href")),
-                EXCEPTEDLINK);
-    }
 }
