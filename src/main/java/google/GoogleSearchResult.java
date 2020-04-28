93859f0ffdbdc224fc436a25b40f24a13edf8eaf
honorzor <ioanntrapeznikov@gmail.com>
2020-04-28 14:05:39 +0300

    Added method "click"

--- src/main/java/google/GoogleSearchResult.java
+++ src/main/java/google/GoogleSearchResult.java
@@ -1,6 +1,7 @@
 package google;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
@@ -8,7 +9,14 @@ public class GoogleSearchResult {
     @FindBy(how = How.CSS, using = "#res .g")
     private ElementsCollection results;
 
+    @FindBy(how = How.CSS, using = ".r >a")
+    private SelenideElement linkToBank;
+
     public ElementsCollection results() {
         return results;
+    }
+
+    public void click() {
+        linkToBank.click();
     }
 }
